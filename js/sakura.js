9a01aa14b0dd18f5b0e3b089c68d2bd78843de4f
朱世胤 <2562603065@qq.com>
2024-02-03 22:47:44 +0800

    Site updated: 2024-02-03 22:47:33

--- js/sakura.js
+++ js/sakura.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const container = document.createElement('div');
     container.className = 'sakura';
 
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 100; i++) {
         const sakura = document.createElement('img');
         sakura.src = 'https://img2.imgtp.com/2024/02/03/nHmmn9Px.jpg'; // 替换为你的樱花图片路径
         sakura.style.left = `${Math.random() * 100}vw`;
