15e5be0312d418d07fb13e064c6043810ed22643
朱世胤 <2562603065@qq.com>
2024-02-15 13:55:00 +0800

    Site updated: 2024-02-15 13:54:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2024/01/17/hello-world/","2024/02/03/博客搭建历程/","2024/01/19/我的第一篇博客blog/","2024/02/06/哇酷哇酷/","2024/01/31/寒假日常/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2024/02/03/博客搭建历程/","2024/01/17/hello-world/","2024/02/06/哇酷哇酷/","2024/01/31/寒假日常/","2024/01/19/我的第一篇博客blog/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
